2d9e20d1cfdbec5b33726e287c81ffcad9b2725c
GermanEduardoVega <german.vega@alumnos.frm.utn.edu.ar>
2024-06-03 08:35:42 -0300

    primer comit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "./views/Home";
+import AdminPage from "./views/AdminPage";
+import ClientPage from "./views/ClientPage";
+import ErrorPage from "./views/ErrorPage";
+import ClientProfilePage from "./views/ClientProfilePage";
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/cliente" element={<ClientPage />} />
+        <Route path="/cliente/perfil" element={<ClientProfilePage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
